524ee5cce98e813889734b4be77b9995840185df
G0dsp <88639130+G0dsp@users.noreply.github.com>
2021-08-09 13:27:35 +0200

    Update EasyNmap.bash
    this is the v1.1

--- EasyNmap.bash
+++ EasyNmap.bash
@@ -12,30 +12,65 @@ echo "
   Made by G0dsp 
                                                      https://github.com/G0dsp"
 sleep 5
-echo ""
-echo "Lets make u a little questions friend"
-echo""
+
+echo " "
+echo "Lets make u a little questions my friend"
+echo " "
 echo "Hello, please introduce the ip that u are going to scan"
 read -r ip
 
-echo -n "Your´s attack mode"
+echo " "
+
+echo "nmap $ip"
+
+echo " "
+
+echo -n "Your´s attack mode  "
 echo ""
 read -r mode
 
-echo -n "How do you want to go fast (-T5)? or slower?(-T1)  "
+echo " "
+
+echo "nmap $ip $mode"
+
+echo " "
+
+echo -n "Want a script? (--script ...)  "
+echo ""
+read -r script
+
+echo " "
+
+echo "nmap $ip $mode $script"
+
+echo " "
+
+echo -n "How do you want to go fast (-T5)? or slower?(-T1)   "
 echo ""
 read -r Ts
 
-echo "Want to see in direct what is going on?"
-echo ""
-echo -n "-v(see what is happening) or press intro"
-read -r verb
+echo " "
 
+echo "nmap $ip $mode $script $Ts"
+
+echo " "
+
+echo ""
+echo -n "-v(see what is happening right now in console) or press intro    "
+echo ""
+read -r verb
+echo " "
+
+echo "nmap $ip $mode $script $Ts $verb"
+sleep 2
 clear
 
-nmap $ip $mode $Ts $verb -oG easynmapscan
-echo""
+nmap $ip $mode $script $Ts $verb -oG easynmapscan
+
+echo " "
 echo "Ey I saved your scan like easynmapscan in a grep format if u want to search something using | grep"
-echo""
+echo " "
 echo "thx 4 use me <3"
-echo "remenber to comment and vote tgis small and first project :D"
+echo "remenber to comment and vote this small and first project :D"
+sleep 3
+echo " "
